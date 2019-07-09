49213ab7ad9f2555a401ff480d9768c8dea36d2b
Anderson Correa <anderson.correa@accenture.com>
2019-07-09 13:36:25 -0300

    atualizando a versão do springboot / alterando o script do gradle para Kotlin DSL

---
+++ build.gradle.kts
@@ -0,0 +1,40 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("plugin.jpa") version "1.2.71"
+    id("org.springframework.boot") version "2.1.6.RELEASE"
+    id("io.spring.dependency-management") version "1.0.7.RELEASE"
+    kotlin("jvm") version "1.2.71"
+    kotlin("plugin.spring") version "1.2.71"
+    kotlin("plugin.allopen") version "1.2.71"
+    kotlin("kapt") version "1.2.71"
+}
+
+group = "com.andersonbco"
+version = "0.0.1-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_1_8
+
+val developmentOnly by configurations.creating
+configurations {
+    runtimeClasspath {
+        extendsFrom(developmentOnly)
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310")
+    runtimeOnly("com.h2database:h2:1.4.197")
+    testImplementation("org.springframework.boot:spring-boot-starter-test") {
+        exclude(module = "junit")
+        exclude(module = "mockito-core")
+    }
+    testImplementation("org.junit.jupiter:junit-jupiter-api")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
+    testImplementation("com.ninja-squad:springmockk:1.1.2")
+}
