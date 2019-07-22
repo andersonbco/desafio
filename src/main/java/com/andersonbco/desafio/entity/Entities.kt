d7be3605696caccfc7d101389469bd1ad76a3e2d
Anderson Correa <anderson.correa@accenture.com>
2019-07-22 17:04:22 -0300

    ajustando a formatação da data de ultimo login / criando o DTO de telefone

--- src/main/java/com/andersonbco/desafio/entity/Entities.kt
+++ src/main/java/com/andersonbco/desafio/entity/Entities.kt
@@ -1,6 +1,7 @@
 package com.andersonbco.desafio.entity
 
 import com.fasterxml.jackson.annotation.JsonFormat
+import com.fasterxml.jackson.annotation.JsonProperty
 import java.time.LocalDateTime
 import javax.persistence.Entity
 import javax.persistence.GeneratedValue
@@ -21,13 +22,19 @@ class User(
         var name: String,
         var email: String,
         var password: String,
-        @JsonFormat(pattern="dd-MM-yyyy") var created: LocalDateTime? = LocalDateTime.now(),
-        @JsonFormat(pattern="dd-MM-yyyy") var modified: LocalDateTime? = LocalDateTime.now(),
-        @JsonFormat(pattern="dd-MM-yyyy") var lastLogin: LocalDateTime? = null
+        @JsonFormat(pattern = "dd-MM-yyyy") var created: LocalDateTime? = LocalDateTime.now(),
+        @JsonFormat(pattern = "dd-MM-yyyy") var modified: LocalDateTime? = LocalDateTime.now(),
+        @JsonFormat(pattern = "dd-MM-yyyy") @JsonProperty("last_login") var lastLogin: LocalDateTime? = LocalDateTime.now()
 )
 
 data class UserDTO(
         val name: String,
         val email: String,
-        val password: String
+        val password: String,
+        val phones: List<PhoneDTO>
+)
+
+data class PhoneDTO(
+        val ddd: Int,
+        val number: Int
 )
