6ad0cbd31b5a83b747fce2a6cb7c231760b12d84
Anderson Correa <anderson.correa@accenture.com>
2019-07-22 17:05:11 -0300

    implementando a busca por usuario no repositorio de telefones

--- src/main/java/com/andersonbco/desafio/repository/Repositories.kt
+++ src/main/java/com/andersonbco/desafio/repository/Repositories.kt
@@ -4,7 +4,9 @@ import com.andersonbco.desafio.entity.Phone
 import com.andersonbco.desafio.entity.User
 import org.springframework.data.repository.CrudRepository
 
-interface PhoneRepository : CrudRepository<Phone, Long>
+interface PhoneRepository : CrudRepository<Phone, Long> {
+    fun findAllByUser(user: User): List<Phone>
+}
 
 interface UserRepository : CrudRepository<User, Long> {
     fun findByEmailIgnoreCase(email: String): User
